4f1429a94ee04c5a9b060a7ddc7558f9d5935b7f
Érico Murta <66687324+ericomurta1991@users.noreply.github.com>
2022-10-19 09:45:13 -0300

    adicionando eventos ao robotron

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", () => {
+    console.log("Cliquei no robo")
+})
+
+function dizOi(nome){
+    console.log(nome)
+    console.log("Bem vindo ao robotron 2000")
+}
+
+dizOi("Érico")
