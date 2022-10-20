71e7e4c61144ca01794d91b9a2d66175c6e823d3
Érico Murta <66687324+ericomurta1991@users.noreply.github.com>
2022-10-20 11:41:26 -0300

    finalizado contador de atributos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-const robotron = document.querySelector("#robotron")
+const controle = document.querySelectorAll("[data-controle]")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+
 
 robotron.addEventListener("click", () => {
     console.log("Cliquei no robo")
@@ -10,3 +40,42 @@ function dizOi(nome){
 }
 
 dizOi("Érico")
+
+1 forma de fazer
+
+somar.addEventListener("click",(evento) => {
+    braco.value = parseInt(braco.value) + 1
+})
+
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1
+})
+
+2 forma de fazer
+
+somar.addEventListener("click", () => {manipulaDados("somar")})
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")} )
+
+3 forma de fazer
+*porem dessa forma o odigo javascript fica atrelado ao css e pode ocasionar falhas se o desing ou alguem mudar o nome dos ids
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector(".controle-contador")
+
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+*/
+
